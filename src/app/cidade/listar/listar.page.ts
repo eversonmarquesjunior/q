1a86870f7949b60269621bcc2a04cbc74797dd15
eversonmarquesjunior <everson.marques370@gmail.com>
2019-09-07 17:32:31 -0300

    pesquisa

--- src/app/cidade/listar/listar.page.ts
+++ src/app/cidade/listar/listar.page.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs';
 import { Estado } from 'src/app/estado/entidade/estado';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { map } from 'rxjs/operators';
+import { Cidade } from '../entidade/cidade';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-listar',
@@ -12,6 +14,10 @@ import { map } from 'rxjs/operators';
 export class ListarPage implements OnInit {
 
   listaCidades: Observable<Estado[]>;
+  listaFiltro: Cidade[];
+  filtro = {};
+  cidades: any;
+  valor: string;
 
   constructor(private fire: AngularFireDatabase) {
     this.listaCidades = this.fire.list<Estado>('cidade').snapshotChanges().pipe(map(lista => lista.map(linha => ({
@@ -20,6 +26,16 @@ export class ListarPage implements OnInit {
   }
 
   ngOnInit() {
+    this.listaCidades.subscribe(cidade => {
+        this.cidades = cidade;
+        this.listaFiltro = _.filter(this.cidades, _.conforms(this.filtro));
+    })
   }
+
+  filtrar(){
+    this.filtro['nome'] = val => val.includes(this.valor);
+    this.listaFiltro = _.filter(this.cidades, _.conforms(this.filtro));
+  }
+
 
 }
